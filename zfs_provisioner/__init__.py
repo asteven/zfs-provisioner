0ac9d3226bd92ce8276c02f26ed36c13a5b94f06
Steven Armstrong <steven.armstrong@id.ethz.ch>
2020-05-12 22:52:48 +0200

    move get_template helper
    Signed-off-by: Steven Armstrong <steven.armstrong@id.ethz.ch>

--- zfs_provisioner/__init__.py
+++ zfs_provisioner/__init__.py
@@ -1,5 +1,12 @@
+import os
 
 class Error(Exception):
     """Base class for all errors in the zfs_provisioner.
     """
     pass
+
+
+def get_template(template_file):
+    template_path = os.path.join(os.path.dirname(__file__), 'templates', template_file)
+    template = open(template_path, 'rt').read()
+    return template
